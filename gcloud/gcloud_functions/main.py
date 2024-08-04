9f52a0ab9fa851e8b02e18b73020445c716e1596
robert-fabijan <robert.f.fabijan@gmail.com>
2024-08-04 21:50:11 +0100

    Test func9

--- gcloud/gcloud_functions/main.py
+++ gcloud/gcloud_functions/main.py
@@ -3,6 +3,10 @@ import sys
 import os
 import requests
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  
+
+from shared.models.gcloud_integration import GCloudIntegration
 
 @functions_framework.http
 def gcloud_download_companieshouse_file(request, context=None) -> None:
@@ -30,17 +34,17 @@ def gcloud_download_companieshouse_file(request, context=None) -> None:
 
     print("File downloaded successfully!")
     
-    print(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+    print()
 
     # # Bucket upload
-    # GCloudIntegrationObject = GCloudIntegration(project_id = 'companieshouse-test') 
+    GCloudIntegrationObject = GCloudIntegration(project_id = 'companieshouse-test') 
     # secret = GCloudIntegrationObject.get_secret(project_id = 'companieshouse-test', secret_id = "credentials-for-authentication")
-    # GCloudIntegrationObject.upload_data_to_cloud_from_file(
-    #     secret = secret,
-    #     bucket_name = 'companieshouse_bucket',
-    #     response = response,
-    #     blob_name = 'raw_files',
-    #     filename = filename
-    # )
+    GCloudIntegrationObject.upload_data_to_cloud_from_file(
+        secret = os.environ['GOOGLE_APPLICATION_CREDENTIALS'],
+        bucket_name = 'companieshouse_bucket',
+        response = response,
+        blob_name = 'raw_files_companieshouse',
+        filename = filename
+    )
     
     return 'Test'
