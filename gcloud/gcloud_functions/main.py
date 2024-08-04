03d516dcd3fc1c18c8f16896c21f6ff5a5a0f9f1
robert-fabijan <robert.f.fabijan@gmail.com>
2024-08-04 22:12:01 +0100

    Test func10

--- gcloud/gcloud_functions/main.py
+++ gcloud/gcloud_functions/main.py
@@ -1,52 +1,37 @@
 import functions_framework
-import sys
 import os
 import requests
-from io import BytesIO
 from google.cloud import storage
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  
-
-from shared.models.gcloud_integration import GCloudIntegration
-
 @functions_framework.http
-def gcloud_download_companieshouse_file(request, context=None) -> None:
+def gcloud_download_companieshouse_file(request, context=None) -> str:
     """
-    Run a process of downloading a csv file from companieshouse endpoint
+    Download a CSV file from Companies House endpoint and upload it to a Google Cloud Storage bucket.
 
     :param request:
     :param context:
-    :return pandas.DataFrame: clean dataframe with air pollution data 
+    :return: Confirmation string indicating successful upload.
     """
-    
     # The URL of the file to download
     url = 'https://download.companieshouse.gov.uk/BasicCompanyDataAsOneFile-2024-08-01.zip'
     
     # Get the filename from the URL
     filename = url.split('/')[-1]
-
-    # Download the file
-    # Create a requests session
-    session = requests.Session()
     bucket_name = 'raw_files_companieshouse'
 
-    #  Get the file content from the URL
-    response = session.get(url, stream=True)
-    if response.status_code == 200:
-        # Read the content into a BytesIO object
-        file_stream = BytesIO(response.content)
-        
-        # Create a storage client
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(filename)
-        
-        # Upload the file content from the BytesIO object
-        blob.upload_from_file(file_stream, rewind=True)
-        
-        print(f"File uploaded to {filename} in bucket {bucket_name}.")
-    else:
-        response.raise_for_status()
+    # Start a session and download the file
+    with requests.Session() as session:
+        with session.get(url, stream=True) as response:
+            response.raise_for_status()  # Check for HTTP errors
 
-    return "True"
+            # Stream the file directly into GCS without saving locally
+            storage_client = storage.Client()
+            bucket = storage_client.bucket(bucket_name)
+            blob = bucket.blob(filename)
+            
+            # Stream directly from the response to GCS
+            blob.upload_from_file(response.raw, content_type='application/zip', rewind=True)
+            print(f"File uploaded to {filename} in bucket {bucket_name}.")
+
+    return "File successfully uploaded."
+
